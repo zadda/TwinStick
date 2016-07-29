295d62bf7dc2aeb90d0e748b1765cd3ec8f902fe
mike_b <atm89@hotmail.com>
2016-07-29 12:30:37 +0200

    add CrossPlatformInput asset pack

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+        // Debug.Log(CrossPlatformInputManager.GetAxis("Horizontal"));
+        // Debug.Log(CrossPlatformInputManager.GetAxis("Vertical"));
+    }
+}
